28d767d02e1cd26624e1db930e10b29fda9ed786
MaxxLarss <elarsson340@gmail.com>
2022-10-12 19:47:46 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const { Sequelize } = require('sequelize')
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'my_password'
+  })
+
 // DEPENDENCIES
 const express = require('express')
 const app = express()
